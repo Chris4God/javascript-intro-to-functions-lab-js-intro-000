c2c03dd7f5b7084403f6f6d7f6d25f45e1b09f5e
Christopher Short <christopher4god@gmail.com>
2018-06-21 20:28:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) 
+{
+  return string
+ .toUpperCase('hello')
+}
+
+function whisper(string)
+{
+  return string
+  .toLowerCase('HELLO')
+}
+
+console.log(logShout)
+  logShout.toUpperCase('hello')
